77d298c2da3ed08fb7a9e390a6438e9b2f5d854e
Collin Montag <cm759@cornell.edu>
2020-03-06 10:28:57 -0500

    ipc enabled flag

--- scripts/3_startnetwork.lua
+++ scripts/3_startnetwork.lua
@@ -1,9 +1,9 @@
 second = 1000000
 cmds = {
-    "startnode a1 --db-enabled=false --ipc-enabled=false --http-port=9655 --staking-port=9155 --xput-server-port=9255 --log-level=verbo --bootstrap-ips=",
-    "startnode a2 --db-enabled=false --ipc-enabled=false --http-port=9656 --staking-port=9156 --xput-server-port=9256 --log-level=verbo --bootstrap-ips=127.0.0.1:9155",
-    "startnode a3 --db-enabled=false --ipc-enabled=false --http-port=9657 --staking-port=9157 --xput-server-port=9257 --log-level=verbo --bootstrap-ips=127.0.0.1:9155,127.0.0.1:9156",
-    "startnode jrpcnode --db-enabled=false --ipc-enabled=true --http-port=9650 --staking-port=9158 --xput-server-port=9258 --log-level=verbo --bootstrap-ips=127.0.0.1:9155,127.0.0.1:9156,127.0.0.1:9157",
+    "startnode a1 --db-enabled=false --api-ipcs-enabled=false --http-port=9655 --staking-port=9155 --xput-server-port=9255 --log-level=verbo --bootstrap-ips=",
+    "startnode a2 --db-enabled=false --api-ipcs-enabled=false --http-port=9656 --staking-port=9156 --xput-server-port=9256 --log-level=verbo --bootstrap-ips=127.0.0.1:9155",
+    "startnode a3 --db-enabled=false --api-ipcs-enabled=false --http-port=9657 --staking-port=9157 --xput-server-port=9257 --log-level=verbo --bootstrap-ips=127.0.0.1:9155,127.0.0.1:9156",
+    "startnode jrpcnode --db-enabled=false --api-ipcs-enabled=true --http-port=9650 --staking-port=9158 --xput-server-port=9258 --log-level=verbo --bootstrap-ips=127.0.0.1:9155,127.0.0.1:9156,127.0.0.1:9157",
 }
 
 for key, cmd in ipairs(cmds) do
