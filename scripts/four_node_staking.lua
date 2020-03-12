68e7a30bd986f81d121255578e3b7e384a52c6c7
Collin Montag <cm759@cornell.edu>
2020-03-12 15:03:18 -0400

    script updates

--- scripts/four_node_staking.lua
+++ scripts/four_node_staking.lua
@@ -3,7 +3,6 @@ cmds = {
     "startnode node2 --staking-tls-enabled=true --http-port=9652 --staking-port=9653 --log-level=verbo --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=certs/keys2/staker.crt --staking-tls-key-file=certs/keys2/staker.key",
     "startnode node3 --staking-tls-enabled=true --http-port=9654 --staking-port=9655 --log-level=verbo --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=certs/keys3/staker.crt --staking-tls-key-file=certs/keys3/staker.key",
     "startnode node4 --staking-tls-enabled=true --http-port=9656 --staking-port=9657 --log-level=verbo --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=certs/keys4/staker.crt --staking-tls-key-file=certs/keys4/staker.key",
-    "startnode node5 --staking-tls-enabled=true --http-port=9658 --staking-port=9659 --log-level=verbo --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=certs/keys5/staker.crt --staking-tls-key-file=certs/keys5/staker.key",
 }
 
 for key, cmd in ipairs(cmds) do
