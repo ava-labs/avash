6d621b966a6d2174fd76cc0976554688a990feeb
Collin Montag <cm759@cornell.edu>
2020-04-10 09:26:16 -0400

    node refactor

---
+++ node/config.go
@@ -0,0 +1,114 @@
+package node
+
+import (
+	"reflect"
+)
+
+// Flags represents available CLI flags when starting a node
+type Flags struct {
+	// Avash metadata
+    ClientLocation               string
+    Meta                         string
+	DataDir                      string
+	
+	// Assertions
+	AssertionsEnabled            bool
+
+	// TX fees
+	AvaTxFee                     uint
+
+	// IP
+	PublicIP                     string
+
+	// Network ID
+	NetworkID                    string
+
+	// Throughput
+	XputServerPort               uint
+
+	// Crypto
+	SignatureVerificationEnabled bool
+
+	// IPC
+	APIIPCsEnabled               bool
+
+	// HTTP
+	HTTPPort                     uint
+	HTTPTLSEnabled               bool
+	HTTPTLSCertFile              string
+	HTTPTLSKeyFile               string
+
+	// Bootstrapping
+	BootstrapIPs                 string
+	BootstrapIDs                 string
+
+	// Database
+	DBEnabled                    bool
+	DBDir                        string
+
+	// Logging
+	LogLevel                     string
+	LogDir                       string
+
+	// Consensus
+	SnowAvalancheBatchSize       int
+	SnowAvalancheNumParents      int
+	SnowSampleSize               int
+	SnowQuorumSize               int
+	SnowVirtuousCommitThreshold  int
+	SnowRogueCommitThreshold     int
+
+	// Staking
+	StakingTLSEnabled            bool
+	StakingPort                  uint
+	StakingTLSKeyFile            string
+	StakingTLSCertFile           string
+}
+
+// SetDefaults sets any zero-value field to its default value
+func (flags *Flags) SetDefaults() {
+	f := reflect.Indirect(reflect.ValueOf(flags))
+	z := reflect.ValueOf(Flags{})
+	d := reflect.ValueOf(DefaultFlags())
+	for i := 0; i < f.NumField(); i++ {
+		if f.Field(i).Interface() == z.Field(i).Interface() {
+			f.Field(i).Set(d.Field(i))
+		}
+	}
+}
+
+// DefaultFlags returns Avash-specific default node flags
+func DefaultFlags() Flags {
+	return Flags{
+        ClientLocation:               "",
+        Meta:                         "",
+        DataDir:                      "",
+		AssertionsEnabled:            true,
+		AvaTxFee:                     0,
+		PublicIP:                     "127.0.0.1",
+		NetworkID:                    "local",
+		XputServerPort:               9652,
+		SignatureVerificationEnabled: true,
+		APIIPCsEnabled:			      true,
+		HTTPPort:                     9650,
+		HTTPTLSEnabled:               false,
+		HTTPTLSCertFile:              "",
+		HTTPTLSKeyFile:               "",
+		BootstrapIPs:                 "",
+		BootstrapIDs:                 "",
+		DBEnabled:                    true,
+		DBDir:                        "db1",
+		LogLevel:                     "info",
+		LogDir:                       "logs",
+		SnowAvalancheBatchSize:       30,
+		SnowAvalancheNumParents:      5,
+		SnowSampleSize:               2,
+		SnowQuorumSize:               2,
+		SnowVirtuousCommitThreshold:  5,
+		SnowRogueCommitThreshold:     10,
+		StakingTLSEnabled:            false,
+		StakingPort:                  9651,
+        StakingTLSKeyFile:            "",
+		StakingTLSCertFile:           "",
+	}
+}
---
+++ node/metadata.go
@@ -0,0 +1,16 @@
+package node
+
+// Metadata struct for storing metadata, available to commands
+type Metadata struct {
+	Serverhost     string `json:"public-ip"`
+	Stakingport    string `json:"staking-port"`
+	HTTPport       string `json:"http-port"`
+	HTTPTLS        bool   `json:"http-tls-enabled"`
+	Dbdir          string `json:"db-dir"`
+	Datadir        string `json:"data-dir"`
+	Logsdir        string `json:"log-dir"`
+	Loglevel       string `json:"log-level"`
+	StakerCertPath string `json:"staking-tls-cert-file"`
+	StakerKeyPath  string `json:"staking-tls-key-file"`
+}
+
