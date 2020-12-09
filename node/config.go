8c5a0d4df47b79693b8909076260b7e579956299
Gabriel Cardona <gabriel@avalabs.org>
2020-12-09 15:58:16 -0800

    Add missing default flag value.

--- node/config.go
+++ node/config.go
@@ -200,5 +200,6 @@ func DefaultFlags() Flags {
 		APIAuthRequired:              false,
 		APIAuthPassword:              "",
 		MinStakeDuration:             "",
+		ConfigFile:                   "",
 	}
 }
