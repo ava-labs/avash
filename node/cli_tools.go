4442f6a1de833aaea0f8a61c71d8ccd2437f4e85
Gabriel Cardona <gabriel@avalabs.org>
2020-12-22 14:03:21 -0800

    Add `--disconnected-check-frequency` flag.

--- node/cli_tools.go
+++ node/cli_tools.go
@@ -96,6 +96,7 @@ func FlagsToArgs(flags Flags, basedir string, sepBase bool) ([]string, Metadata)
 		"--ipcs-path=" + flags.IPCSPath,
 		"--fd-limit=" + strconv.Itoa(flags.FDLimit),
 		"--benchlist-fail-threshold=" + strconv.Itoa(flags.BenchlistFailThreshold),
+		"--disconnected-check-frequency=" + flags.DisconnectedCheckFrequency,
 	}
 	if sepBase {
 		args = append(args, "--data-dir="+basedir)
