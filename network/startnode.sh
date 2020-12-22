440120580f6ee7eebc0b884c22c49a73a3c7533b
Gabriel Cardona <gabriel@avalabs.org>
2020-12-22 13:48:14 -0800

    Add `--network-timeout-reduction` flag.

--- network/startnode.sh
+++ network/startnode.sh
@@ -45,6 +45,7 @@ do
 		--snow-quorum-size=*|\
 		--snow-virtuous-commit-threshold=*|\
 		--snow-rogue-commit-threshold=*|\
+		--network-timeout-reduction=*|\
         --p2p-tls-enabled=*|\
 		--staking-enabled=*|\
 		--staking-tls-key-file=*|\
