9083613d7596fab6e70d90acb861d1b367128a47
Gabriel Cardona <gabriel@avalabs.org>
2020-12-30 09:50:24 -0800

    Add default value.

--- cmd/startnode.go
+++ cmd/startnode.go
@@ -145,7 +145,7 @@ func init() {
 	StartnodeCmd.Flags().IntVar(&flags.SnowQuorumSize, "snow-quorum-size", flags.SnowQuorumSize, "Alpha value to use for required number positive results.")
 	StartnodeCmd.Flags().IntVar(&flags.SnowVirtuousCommitThreshold, "snow-virtuous-commit-threshold", flags.SnowVirtuousCommitThreshold, "Beta value to use for virtuous transactions.")
 	StartnodeCmd.Flags().IntVar(&flags.SnowRogueCommitThreshold, "snow-rogue-commit-threshold", flags.SnowRogueCommitThreshold, "Beta value to use for rogue transactions.")
-	StartnodeCmd.Flags().IntVar(&flags.SnowEpochFirstTransition, "snow-epoch-first-transition", flags.SnowEpochFirstTransition, "Unix timestamp of the transition from epoch 0 to epoch 1")
+	StartnodeCmd.Flags().IntVar(&flags.SnowEpochFirstTransition, "snow-epoch-first-transition", flags.SnowEpochFirstTransition, "Unix timestamp of the transition from epoch 0 to epoch 1. Defaults to 1609873200 which is 1/5/2021 @ 7:00pm (UTC)")
 
 	StartnodeCmd.Flags().BoolVar(&flags.P2PTLSEnabled, "p2p-tls-enabled", flags.P2PTLSEnabled, "Require TLS to authenticate network communications")
 	StartnodeCmd.Flags().BoolVar(&flags.StakingEnabled, "staking-enabled", flags.StakingEnabled, "Enable staking. If enabled, Network TLS is required.")
