7d2b65bb4c6c0a1dbb11ef111db829f34d0a0b11
Gabriel Cardona <gabriel@avalabs.org>
2021-03-24 07:44:24 -0700

    Add network-health-max-portion-send-queue-full flag.

--- network/startnode.sh
+++ network/startnode.sh
@@ -65,6 +65,7 @@ do
 		--network-initial-timeout=*|\
 		--network-minimum-timeout=*|\
 		--network-maximum-timeout=*|\
+		--network-health-max-portion-send-queue-full=*|\
 		--restart-on-disconnected=*|\
 		--disconnected-check-frequency=*|\
 		--disconnected-restart-timeout=*|\
